60916553c94b73864f34363051fb55dc9adf1522
gracewainaina <47871572+gracewainaina@users.noreply.github.com>
2022-04-22 18:34:35 -0500

    tweaking around

--- js/pokemon-app.js
+++ js/pokemon-app.js
@@ -5,6 +5,8 @@ const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056])
 // const NU_CENTER = ol.proj.fromLonLat([-87.6813, 42.049])
 const AUTOMOVE_SPEED = 1
 const UPDATE_RATE = 100
+
+//add a const to track your checkpoints/unlocked landmarks
 /*
  Apps are made out of a header (title/controls) and footer
  and some number of columns
@@ -17,7 +19,29 @@ let landmarkCount = 0
 let gameState = {
 	points: 0,
 	captured: [],
-	messages: []
+	messages: [],
+	ticks: 0,
+	pointRate: 1,
+	healthPoints: 300,
+	maxHealthPoints: 300,
+}
+
+let badPlaces = [
+	ol.proj.fromLonLat([-87.6715, 42.055]),
+	ol.proj.fromLonLat([-87.6745, 42.052]),
+	ol.proj.fromLonLat([-87.6745, 42.059]),
+	ol.proj.fromLonLat([-87.6763, 42.056])
+]
+
+function isBad(location) {
+	for (let badPlace of badPlaces) {
+		if (badPlace[0]-100 <= location[0] &&
+			location[0] <= badPlace[0]+100 &&
+			badPlace[1]-100 <= location[1] &&
+			location[1] <= badPlace[1]+100)
+			return true;
+	}
+	return false;
 }
 
 // Create an interactive map
@@ -31,17 +55,17 @@ let map = new InteractiveMap({
 
 	initializeMap() {
 		// A good place to load landmarks
-		this.loadLandmarks("landmarks-shop-evanston", (landmark) => {
-			// Keep this landmark?
+		// this.loadLandmarks("landmarks-natural-evanston", (landmark) => {
+		// 	// Keep this landmark?
 
-			// Keep all landmarks in the set
-			return true
+		// 	// Keep all landmarks in the set
+		// 	return true
 
-			// Only keep this landmark if its a store or amenity, e.g.
-			// return landmark.properties.amenity || landmark.properties.store
-		})
+		// 	// Only keep this landmark if its a store or amenity, e.g.
+		// 	// return landmark.properties.amenity || landmark.properties.store
+		// })
 
-		// Create random landmarks
+		// Create random landmarks (theses are dummy vars)
 		// You can also use this to create trails or clusters for the user to find
 		for (var i = 0; i < 10; i++) {
 
@@ -57,6 +81,23 @@ let map = new InteractiveMap({
 
 	update() {
 		// Do something each frame
+		//does the update of how many points you have unlocked go here?
+		gameState.ticks += 1;
+		if (gameState.ticks % 10 === 0){
+			gameState.points += gameState.pointRate;
+
+			// player has started exploring, begin decrementing health
+			if(gameState.captured.length > 0){
+				gameState.healthPoints -= 1
+			}
+
+			// also decrement the landmarks down to zero over time
+			// this.landmarks.forEach(landmark => {
+			// 	if (landmark.points > 0){
+			// 		landmark.points -= 1;
+			// 	}
+			// });
+		}
 	},
 
 	initializeLandmark: (landmark, isPlayer) => {
@@ -74,7 +115,14 @@ let map = new InteractiveMap({
 		landmark.color = [Math.random(), 1, .5]
 
 		// Give it a random number of points
-		landmark.points = Math.floor(Math.random()*10 + 1)
+		//how to give a landmark negative points? 
+		// increased point count since im too lazy to make them decrease by minute or smth
+		console.log(isBad(landmark.pos))
+		if (isBad(landmark.pos))
+			//landmark.points = -10;
+			landmark.points = Math.floor(Math.random()*-10);
+		else
+			landmark.points = Math.floor(Math.random()*10 + 1);
 		return landmark
 	}, 
 
@@ -87,7 +135,7 @@ let map = new InteractiveMap({
 
 			// Add points to my gamestate
 			gameState.points += landmark.points
-
+			//gameState.pointRate += 1
 			
 
 			// Have we captured this?
@@ -141,8 +189,6 @@ let map = new InteractiveMap({
 
 
 window.onload = (event) => {
-
-
 	const app = new Vue({
 		template: `
 		<div id="app">
@@ -153,6 +199,11 @@ window.onload = (event) => {
 					(TODO, add your own gamestate)
 					{{gameState}}
 					
+				</div>
+				
+				<div>
+					<button v-on:click="heal">Get more health! (30 points)</button>
+					<button>Get more landmarks! (30 points)</button>
 				</div>
 
 				<div class="main-column" style="overflow:hidden;width:${MAP_SIZE}px;height:${MAP_SIZE}px">
@@ -169,6 +220,15 @@ window.onload = (event) => {
 			
 				map: map,
 				gameState: gameState
+			}
+		},
+
+		methods: {
+			heal: function(event){
+				if (gameState.points >= 30){
+					gameState.points = -30;
+					gameState.healthPoints = Math.min(300, gameState.healthPoints + 60);
+				}
 			}
 		},
 
